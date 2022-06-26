8dbdf48df1958bfcc6d1488f40ff15194f77ff90
Gustavo <gug994@gmail.com>
2022-06-26 17:49:34 -0300

    finished desafio-de-codigo1

---
+++ mod3-javascript/desafio-de-codigo1/script.js
@@ -0,0 +1,11 @@
+let input = [100, 2, 2];
+let n, x, y; 
+
+n =  input[0]; 
+x =  input[1]; 
+y =  input[2]; 
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+let resultado = n / (x + y); 
+console.log(resultado);
